ef917b8ac48594f1da46e8377dafadba6c3da536
ZTXeng <66939598+ZTXeng@users.noreply.github.com>
2023-09-18 11:10:50 +0800

    11

---
+++ WPFlnkPG/DPHelper.cs
@@ -0,0 +1,58 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFlnkPG
+{
+    public static class DPHelper
+    {
+        //填入自己数据库的账号和密码
+        private static string ConnectionString = "Server=localHost;database=postgres;uid=postgres;Password=123456";
+
+        private static Npgsql.NpgsqlConnection Con
+        {
+            get
+            {
+                var conn = new Npgsql.NpgsqlConnection(ConnectionString);
+                conn.Open();
+                return conn;
+            }
+        }
+
+        public static NpgsqlCommand Cmd => Con.CreateCommand();
+
+        public static NpgsqlDataReader SelectForReader(string sql)
+        {
+            var cmd = Cmd;
+            cmd.CommandText = sql;
+            try
+            {
+                return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            }
+            catch (Exception)
+            {
+
+                cmd.Connection.Close();
+                return null;
+            }
+        }
+
+        public static bool Updata(string sql)
+        {
+            var cmd = Cmd;
+            cmd.CommandText = sql;
+            try
+            {
+                return cmd.ExecuteNonQuery() > 0;
+            }
+            finally 
+            {
+
+                cmd.Connection.Close();
+            }
+        }
+    }
+}
---
+++ WPFlnkPG/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFlnkPG
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //reader用来填入查询sql语句
+            var data = DPHelper.SelectForReader("SELECT * FROM public.revituser");
+            //Updata用来填入 增删改语句
+            DPHelper.Updata("INSERT INTO revituser (userid, password, lastlogtime, onlinestateid, level, onlinetime) VALUES ('18', '4455', '2023-07-19 10:00:00', 1, 3, '2023-07-19 10:00:00')");
+            var dt = new DataTable();
+            dt.Load(data);
+            grid.ItemsSource = dt.DefaultView;
+        }
+    }
+}
